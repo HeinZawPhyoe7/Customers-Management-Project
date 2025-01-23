63c57d775a8cfa6e6d9fe83194bb8069b3c24c83
Hein12345 <heinzawphyoe2@gmail.com>
2025-01-23 22:21:43 +0700

    feat: new changes code

---
+++ app/Http/Controllers/CustomerApiController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+
+        return response()->json([
+            'customers'=> $customers,
+            'message'  => 'success',
+            'code'     => 200
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->password = $request->password;
+        $customer->phone = $request->phone;
+        $customer->adress = $request->adress;
+        $customer->role = $request->role;
+        $customer->date_of_birth = $request->date_of_birth;
+        $customer->save();
+        
+        return response()->json([
+            'message' => 'Customer is successfully stored',
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
